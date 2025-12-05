88133a107d61b6b8380ee8d6974d1ebabdae8d94
raphaelmailhiot <raphmailhiot@gmail.com>
2025-12-05 16:45:16 -0500

    Add dashboard

---
+++ dashboard/src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import thyngoLogo from '/thyngo-dark.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://raph.mailhiotinc.com" target="_blank">
+          <img src={thyngoLogo} className="logo" alt="thynGo logo" />
+        </a>
+      </div>
+      <h1>Dashboard pour <span style={{fontWeight: "300", fontSize: "4rem"}}>ThynGo</span></h1>
+
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
